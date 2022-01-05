9b160680579d53fb0e26632c2d2882956f163bee
LLIEV <lliev2704@gmail.com>
2022-01-05 10:27:00 +0200

    added switch

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import "fmt"
 		fmt.Println(i)
 		 
 }
-*/
+
 func main() {
 	for i := 1; i <= 100; i++ { 
 		if i % 3 == 0 && i % 5 == 0 { 
@@ -28,6 +28,17 @@ func main() {
 			fmt.Println("Fizz") 
 		} else {
 			fmt.Println(i) 
+		}
+	}
+}
+*/
+func main() {
+	for i := 1; i <= 100; i++ { 
+		switch {
+			case i % 3 == 0 && i % 5 == 0: fmt.Println(i, "FizzBuzz")
+			case i % 3 == 0: fmt.Println(i, "Fizz")
+			case i % 5 == 0: fmt.Println(i, "Buzz")
+			default: fmt.Println(i) 
 		}
 	}
 }
